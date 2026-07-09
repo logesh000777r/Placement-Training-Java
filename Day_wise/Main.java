6594029ba70b9dcaac116ee658c07a85e1d0d323
logesh000777r <logesh000777r@gmail.com>
2026-07-09 17:12:47 +0530

    Day10..

--- Day_wise/Main.java
+++ Day_wise/Main.java
@@ -5,8 +5,11 @@ package Day_wise;
 //Iteration - For,ForEach,Iterator
 
 //*Day!0 */
-//Map - (HashMap,LinkedHashmap)
+//Map - (HashMap,LinkedHashmap,TreeMap)
 //iteration
+//Queue -(Queue Methods,PriorityQueue,Iterator,ListIterator,Generics,Comparable,Comparator,Stream)
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
