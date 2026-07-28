658a806d9fbdf6275ac27c70411aea4f2ed004c0
logesh000777r <logesh000777r@gmail.com>
2026-07-28 19:56:29 +0530

    Dayy24

---
+++ LeetcodeProblems/ClimbingStairs.java
@@ -0,0 +1,21 @@
+package LeetcodeProblems;
+//LeetCode Problem no : 70
+import java.util.*;
+public class ClimbingStairs {
+    static int climbStairs(int n){
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(climbStairs(n));
+
+    }
+
+}
