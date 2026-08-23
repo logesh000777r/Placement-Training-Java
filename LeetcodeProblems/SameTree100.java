ed8aeb9989ac633f2e9306ce1208f8d535b0c7f9
logesh000777r <logesh000777r@gmail.com>
2026-08-23 21:21:14 +0530

    LeetCode

---
+++ LeetcodeProblems/SameTree100.java
@@ -0,0 +1,27 @@
+package LeetcodeProblems;
+
+import javax.swing.tree.TreeNode;
+
+public class SameTree100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+    //     // Both nodes are null
+    //     if (p == null && q == null) {
+    //         return true;
+    //     }
+        
+    //     // One node is null
+    //     if (p == null || q == null) {
+    //         return false;
+    //     }
+        
+    //     // Values are different
+    //     if (p.val != q.val) {
+    //         return false;
+    //     }
+        
+    //     // Check left and right subtrees
+    //     return isSameTree(p.left, q.left) &&
+    //            isSameTree(p.right, q.right);
+    // }
+}
